e3ce5e787beea41ec7dafce03bdcefdd001f99d9
Anh Do <dinhanhdo95@gmail.com>
2020-09-06 11:18:59 +0700

    Build chat messages

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -3,6 +3,9 @@ import { Avatar, IconButton } from '@material-ui/core'
 import SearchIcon from '@material-ui/icons/Search'
 import AttachFileIcon from '@material-ui/icons/AttachFile';
 import MoreVertIcon from '@material-ui/icons/MoreVert'
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon'
+import MicIcon from '@material-ui/icons/Mic'
+import SendIcon from '@material-ui/icons/Send'
 
 import './styles.scss'
 
@@ -29,7 +32,34 @@ function Chat() {
       </header>
 
       <div className="chat__body">
+        <ul className="chat__body-messages">
+          <li className="chat__body-message">
+            <strong className="chat__body-name">David</strong>
+            <p className="chat__body-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <span className="chat__body-timestamp">
+              {new Date().toUTCString()}
+            </span>
+          </li>
+          <li className="chat__body-message chat__body-message--text-right">
+            <strong className="chat__body-name">Richard Do</strong>
+            <p className="chat__body-text chat__body-text--highlight">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <span className="chat__body-timestamp">
+              {new Date().toUTCString()}
+            </span>
+          </li>
+        </ul>
       </div>
+
+      <footer className="chat__footer">
+        <IconButton><InsertEmoticonIcon /></IconButton>
+        <form className="chat__footer-comment-box">
+          <input className="chat__footer-comment-input" type="text" placeholder="Type a message" />
+          <button className="chat__footer-send-btn" type="submit">
+            <SendIcon />
+          </button>
+        </form>
+        <IconButton><MicIcon /></IconButton>
+      </footer>
     </div>
   )
 }
